a2600273a5ccb657d799d5da501469a3b00099e5
DDe3 <lasalinasb@uce.edu.ec>
2022-01-13 19:36:35 -0500

    taller12

--- 02vue/app.js
+++ 02vue/app.js
@@ -14,7 +14,27 @@ const myApp = Vue.createApp({
     data() {
         return {
             mensaje: "Hola mundo",
-            miArreglo: personas
+            miArreglo: personas,
+            nuevoNombre: "Valor Inicial",
+            nuevaEdad : "0"
+        }
+    },
+    methods: {
+        metodoTeclado() {
+            //console.log(this.nuevaPropiedad)
+            //console.log(event)
+            this.miArreglo.unshift( { nombre: this.nuevoNombre, edad: this.nuevaEdad })
+
+            //charCode == 13 ? this.miArreglo.unshift( { nombre: this.nuevaPropiedad }) : false
+            // Metodo Push para agregar elemento al final
+            //this.miArreglo.push( {
+            //    nombre: this.nuevaPropiedad,
+            //    edad: "Nuevo Dato"
+            //})
+
+
+            // Metodo Unshift para agregar elemento al inicio
+            
         }
     }
 
